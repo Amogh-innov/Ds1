2fae311bcc6fceb6d9f54a7b52efa559bf617b16
Amogh <amoghsrcm@gmail.com>
2018-03-05 00:00:08 +0530

    Update PokDs.py

--- PokDs.py
+++ PokDs.py
@@ -1,34 +1,39 @@
-import pandas as pnd
-import matplotlib.pyplot as plo
+def Ack():  # Acknowledgement about the code
+    print("\nData science program")
+    print("Pokemon Dataset , source : https://www.kaggle.com/abcsds/pokemon/data")
 
-print("\nData science program")
-print("Pokemon Dataset , source : https://www.kaggle.com/abcsds/pokemon/data")
-ofil = pnd.read_csv("Pokemon.csv")
+def Opn(file):  # Opens csv file in read mode
+    import pandas as pnd
+    return pnd.read_csv(file)
 
-print("\n\n Mean of dataset \n")
-print(ofil.mean())
+def PriDa(ofil):  # Finds mean median mode of 'ofil'
+    import matplotlib.pyplot as plo
+    print("\n\n Mean of dataset \n")
+    print(ofil.mean())  #Displays mean of dataset
+    print("\n\n Median of dataset \n")
+    print(ofil.median())  #Displays median of dataset
+    print("\n\n Mode of dataset")
+    print(ofil.mode())  #Displays mode of dataset
 
-print("\n\n Median of dataset \n")
-print(ofil.median())
+def PriGph(ofil):  # Plots different graphs on 'ofil'
+    import matplotlib.pyplot as plo
+    print("Graph plots will be shown \n")
+    print("Figure 1 - General plotting of data on graph")
+    ofil.plot()  # Plots all columns against index graph
+    print("Figure 2 - Scatter graph with x axis as Attack and y axis as Defence")
+    ofil.plot(kind='scatter',x='Attack',y='Defense') # Plots scatter graph
+    print("Figure 3 - Density graph")
+    ofil.plot(kind='density')  # Plots estimate density function graph
+    print("Figure 4 - Histogram graph")
+    ofil.plot(kind='hist') # Plots histogram graph
+    print("Figure 5 - Box graph")
+    ofil.plot(kind='box') # plots Box graph
+    plo.show() #visualization
 
-print("\n\n Mode of dataset")
-print(ofil.mode())
+def Menu():  # Calls all the functions 
+    Ack()
+    ofile = Opn("Pokemon.csv")
+    PriDa(ofile)
+    PriGph(ofile)
 
-print("Graph plots will be shown \n")
-
-print("Figure 1 - General plotting of data on graph")
-ofil.plot()  # plots all columns against index
-
-print("Figure 2 - Scatter graph with x axis as Attack and y axis as Defence")
-ofil.plot(kind='scatter',x='Attack',y='Defense') # scatter plot
-
-print("Figure 3 - Density graph")
-ofil.plot(kind='density')  # estimate density function
-
-print("Figure 4 - Histogram graph")
-ofil.plot(kind='hist') # histogram plot
-
-print("Figure 5 - Box graph")
-ofil.plot(kind='box') # Box plot
-
-plo.show() #visualization
+Menu()
